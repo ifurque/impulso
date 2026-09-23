06994423a35b885088036899fccf2b957e892372
ifurque <ifurque26@gmail.com>
2026-09-23 17:04:02 -0300

    Limpiar duplicados: dejar unicamente el proyecto impulso en el repositorio

--- resources/views/emails/appointment-confirmation.blade.php
+++
@@ -1,5 +0,0 @@
-<h1>Confirma tu turno</h1>
-<p>Hola {{ $appointment->guest_name }}, recibimos tu solicitud para {{ $appointment->business->name }}.</p>
-<p>Fecha: {{ $appointment->appointment_date->format('d/m/Y') }} a las {{ $appointment->start_time }}</p>
-<p><a href="{{ route('appointments.confirm', $appointment->confirmation_token) }}">Confirmar solicitud de turno</a></p>
-<p>También podés <a href="{{ route('appointments.manage', $appointment->management_token) }}">cancelar o reprogramar tu turno</a>.</p>
